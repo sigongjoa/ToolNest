af80815096129b383df29d542b64593d91aa86ff
sigongjoa <zeskywa499@gmail.com>
2025-06-23 16:54:46 +0900

    Deploy:

--- ToolReviewHub/vite.config.ts
+++ ToolReviewHub/vite.config.ts
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/ToolNest/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
